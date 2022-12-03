3367b90ef82ce531dd27e7fd6cca8f3ecf1b4217
mariazolcik <s29464@pjwstk.edu.pl>
2022-12-03 15:33:13 +0100

    Koniev

--- Zadanie04.java
+++ Zadanie04.java
@@ -2,25 +2,33 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Zadanie04 {
-    public static int ADD(int zmienna1,int zmienna2){
-        int ADD= zmienna1+zmienna2;
+    public static double ADD(double zmienna1,double zmienna2){
+        double ADD= zmienna1+zmienna2;
         return ADD;
     }
-    public static int SUB(int zmienna1,int zmienna2){
-        int SUB= zmienna1-zmienna2;
+    public static double SUB(double zmienna1,double zmienna2){
+        double SUB= zmienna1-zmienna2;
         return SUB;
     }
-    public static int DIV(int zmienna1,int zmienna2){
-        int DIV= zmienna1/zmienna2;
+    public static double DIV(double zmienna1,double zmienna2){
+        double DIV= zmienna1/zmienna2;
         return DIV;
     }
-    public static int MUL(int zmienna1,int zmienna2){
-        int MUL= zmienna1*zmienna2;
+    public static double MUL(double zmienna1,double zmienna2){
+        double MUL= zmienna1*zmienna2;
         return MUL;
     }
     public static double roundDec(double value) {
         return Math.round(value * 100.0) / 100.0;
     }
+
+    public static double absoluteValue(double zmienna){
+        if (zmienna<0){
+            zmienna=(-1)*zmienna;
+        }
+        return zmienna;
+    }
+
 
 
 
@@ -29,7 +37,6 @@ public class Zadanie04 {
         Random r = new Random();
 
 //zadanie1
-
         System.out.println("Czy pada deszcz?");
         boolean czyPada=scan.nextBoolean();
         System.out.println("Czy świeci słońce?");
@@ -45,29 +52,52 @@ public class Zadanie04 {
         }
 
 //zadanie4
-
         System.out.println("Wprowadź dwie zmienne:");
-        int zmienna1= scan.nextInt();
-        int zmienna2= scan.nextInt();
+        double zmienna1= scan.nextDouble();
+        double zmienna2= scan.nextDouble();
         System.out.println("Wybierz polecenie z pośród: ADD,SUB,DIV,MUL");
         String polecenie= scan.next();
-        if(polecenie.equals("ADD")){
-            System.out.println(ADD(zmienna1,zmienna2));
-        } else if(polecenie.equals("SUB")){
-            System.out.println(SUB(zmienna1,zmienna2));
-        } else if(polecenie.equals("DIV")){
-            System.out.println(DIV(zmienna1,zmienna2));
-        } else if(polecenie.equals("MUL")){
-            System.out.println(MUL(zmienna1,zmienna2));
-        } else{
-            System.out.println("Nieprawidłowe polecenie!");
+        switch (polecenie){
+            case "ADD":
+                System.out.println("Wynik: "+ (int)ADD(zmienna1,zmienna2));
+                break;
+            case "SUB":
+                System.out.println("Wynik: "+ (int)SUB(zmienna1,zmienna2));
+                break;
+            case "DIV":
+                System.out.println("Wynik: "+ (int)DIV(zmienna1,zmienna2));
+                break;
+            case "MUL":
+                System.out.println("Wynik: "+ (int)MUL(zmienna1,zmienna2));
+                break;
+            default:
+                System.out.println("Nieprawidłowe polecenie!");
         }
 
         //zadanie5
-
+        System.out.println("Wprowadź dwie zmienne:");
+        double zmienna3= scan.nextDouble();
+        double zmienna4= scan.nextDouble();
+        System.out.println("Wybierz polecenie z pośród: ADD,SUB,DIV,MUL");
+        String polecenie2= scan.next();
+        switch (polecenie2){
+            case "ADD":
+                System.out.println("Wynik: "+ (int)ADD(absoluteValue(zmienna3),absoluteValue(zmienna4)));
+                break;
+            case "SUB":
+                System.out.println("Wynik: "+ (int)SUB(absoluteValue(zmienna3),absoluteValue(zmienna4)));
+                break;
+            case "DIV":
+                System.out.println("Wynik: "+ (int)DIV(absoluteValue(zmienna3),absoluteValue(zmienna4)));
+                break;
+            case "MUL":
+                System.out.println("Wynik: "+ (int)MUL(absoluteValue(zmienna3),absoluteValue(zmienna4)));
+                break;
+            default:
+                System.out.println("Nieprawidłowe polecenie!");
+        }
 
         //zadanie6
-
         System.out.println("Wprowadż 2 liczby rzeczywiste i naciśnij ENTER po każdej z nich:");
         double a= scan.nextDouble();
         double b= scan.nextDouble();
@@ -85,7 +115,6 @@ public class Zadanie04 {
         System.out.println(d2);
         d3= roundDec(r.nextDouble(b-a)+a);
         System.out.println(d3);
-
         if(d1<d2 && d2<d3){
             System.out.println("Gdzie:"+d1+"< "+d2+"< "+d3);
         } else if(d1<d3 && d3<d2){
@@ -102,7 +131,6 @@ public class Zadanie04 {
 
         }else if(d3<d1 && d1<d2){
             System.out.println("Gdzie:"+d3+"< "+d1+"< "+d2);
-
         }
 
         //zadanie8
@@ -122,6 +150,5 @@ public class Zadanie04 {
         } else if (waluta!=1 && waluta!=2){
             System.out.println("Nie mamy takiej waluty");
         }
-
     }
 }
